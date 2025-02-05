0525c6f869c417726402327f9a4115dc7f24ffdb
AtheoJester23 <atheojester@gmail.com>
2025-02-05 11:58:37 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+import NavbarVue from "./components/Navbar.vue";
+
+import { RouterView } from "vue-router";
+
+  const aData = ref("Remember that this is from App.vue");
+</script>
+
+<template>
+  <NavbarVue/>
+  <RouterView/>
+  <h1 class="text-2xl text-center">{{aData}}</h1>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import HeroVue from "@/components/Hero.vue";
+    import HomeCardsVue from "@/components/HomeCards.vue";
+    import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+    <HeroVue title="This is title" daSubtitle="Lorem ipsum something something"/>
+    <HomeCardsVue/>  
+    <JobListings :limit="3" :showButton="true"/>
+</template>
